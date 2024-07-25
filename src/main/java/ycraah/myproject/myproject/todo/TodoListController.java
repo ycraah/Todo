90034d29dc8b533cc79a32714f1a3f6fae79b720
ycraah <ycraah@gmail.com>
2024-07-25 18:02:40 +0900

    list.jsp 구현을 위해 TodoListController 수정, list.jsp 수정

--- src/main/java/ycraah/myproject/myproject/todo/TodoListController.java
+++ src/main/java/ycraah/myproject/myproject/todo/TodoListController.java
@@ -1,16 +1,25 @@
 package ycraah.myproject.myproject.todo;
 
+import ycraah.myproject.myproject.dto.TodoDTO;
+import ycraah.myproject.myproject.service.TodoService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name="todoListController", urlPatterns="/todo/list")
 public class TodoListController extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    List<TodoDTO> dtoList = TodoService.INSTANCE.getList();
+
+    req.setAttribute("list", dtoList);
+
     req.getRequestDispatcher("/WEB-INF/todo/list.jsp").forward(req, resp);
   }
 }
