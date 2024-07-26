b6a2cf981c27b793f0577c56b075a68c1db595d0
ycraah <102202147+ycraah@users.noreply.github.com>
2024-07-26 20:59:14 +0900

    SQL 처리를 전담하는 TodoDAO

---
+++ TodoDAO.java
@@ -0,0 +1,36 @@
+package ycraah.myproject.jdbcex.dao;
+
+import lombok.Cleanup;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class TodoDAO {
+  public String getTime(){
+    String now = null;
+    try(Connection connection = ConnectionUtil.INSTANCE.getConnection();
+    PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+    ResultSet resultSet = preparedStatement.executeQuery();
+    ) {
+      resultSet.next();
+      now = resultSet.getString(1);
+    } catch(Exception e){
+      e.printStackTrace();
+    }
+    return now;
+  }
+
+  public String getTime2() throws Exception {
+    @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+    @Cleanup PreparedStatement preparedStatement = connection.prepareStatement("select now() from dual");
+    @Cleanup ResultSet resultSet = preparedStatement.executeQuery();
+
+    resultSet.next();
+
+    String now = resultSet.getString(1);
+    return now;
+  }
+
+
+}
